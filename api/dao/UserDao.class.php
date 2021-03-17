63e5a78e7ef843bf0c7e8349e74d1e6eb2e7d085
KospoAmela <kospoamela1@gmail.com>
2021-03-17 16:43:21 +0100

    dynamic update method

--- api/dao/UserDao.class.php
+++ api/dao/UserDao.class.php
@@ -19,4 +19,23 @@ class UserDao extends BaseDao
       $stmt= $this->connection->prepare($sql);
       $stmt->execute($user);
     }
+
+    public function updateUser($id, $user)
+    {
+      
+      $query = "UPDATE users SET ";
+      foreach($user as $name => $value){
+        $query .= $name. "=:".$name.", ";
+      }
+      $query = substr($query, 0, -2);
+      $query .= " WHERE user_id=:id";
+      $stmt = $this->connection->prepare($query);
+      $user['id'] = $id;
+      $stmt->execute($user);
+
+      /*$sql = "UPDATE users SET name=:name, surname=:surname, username=:username, password=:password, email=:email WHERE user_id=:id";
+      $stmt = $this->connection->prepare($sql);
+      $user['id'] = $id;
+      $stmt->execute($user);*/
+    }
   }
