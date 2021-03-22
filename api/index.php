25c17b4d729771fb5db5912c2cdcb66c9b8bb161
KospoAmela <kospoamela1@gmail.com>
2021-03-22 19:03:55 +0100

    fixed branch issue

--- api/index.php
+++ api/index.php
@@ -10,44 +10,26 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 Flight::register('userDao', 'UserDao');
-<<<<<<< HEAD
 Flight::register('jobDao', 'JobDao');
 
 Flight::route('GET /users', function(){
     $users = Flight::userDao()->getAllUsers();
-=======
-
-Flight::route('GET /users', function(){
-    $dao = new UserDao();
-    $users = $dao->getAllUsers();
->>>>>>> b748030d0f3b1e831cde1ba80e282f4e05a1c43c
     Flight::json($users);
 });
 
 Flight::route('GET /users/@id', function($id){
-<<<<<<< HEAD
     $user = Flight::userDao()->getUserById($id);
-=======
-    $dao = new UserDao();
-    $user = $dao->getUserById($id);
->>>>>>> b748030d0f3b1e831cde1ba80e282f4e05a1c43c
     Flight::json($user);
 });
 
 Flight::route('POST /users', function(){
     $request = Flight::request();
     $data = $request->data->getData();
-<<<<<<< HEAD
     $user = Flight::userDao()->insertUser($data);
-=======
-    $userDao = new UserDao();
-    $user = $userDao->insertUser($data);
->>>>>>> b748030d0f3b1e831cde1ba80e282f4e05a1c43c
     print_r($data);
 });
 
 Flight::route('PUT /users/@id', function($id){
-<<<<<<< HEAD
     $user = Flight::userDao()->getUserById($id);
     $request = Flight::request();
     $data = $request->data->getData();
@@ -76,22 +58,6 @@ Flight::route('PUT /jobs/@id', function($id){
     $request = Flight::request();
     $data = $request->data->getData();
     $user = Flight::jobDao()->updateJob($id, $data);
-=======
-    $dao = new UserDao();
-    $user = $dao->getUserById($id);
-
-    $request = Flight::request();
-    $data = $request->data->getData();
-
-    $user = $dao->updateUser($id, $data);
-
-});
-
-Flight::route('/jobs', function(){
-    $dao = new JobDao();
-    $jobs = $dao->getAllJobs();
-    Flight::json($jobs);
->>>>>>> b748030d0f3b1e831cde1ba80e282f4e05a1c43c
 });
 
 Flight::route('/hello4', function(){
