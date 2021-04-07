503f1505b6007bbba8e0437d518999755d580a92
KospoAmela <kospoamela1@gmail.com>
2021-04-07 09:30:41 +0200

    swag

--- api/routes/JobRoutes.php
+++ api/routes/JobRoutes.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * @OA\Info(title="Webprogramming project", version="0.1")
+ */
+
+/**
+ * @OA\Get(path="/jobs",
+ *     @OA\Response(response="200", description="List jobs from database")
+ * )
+ */
 Flight::route('GET /jobs', function(){
     $offset = Flight::query("offset", 0);
     $limit = Flight::query("limit", 30);
@@ -6,12 +15,23 @@ Flight::route('GET /jobs', function(){
     Flight::json(Flight::jobService()->getJobs($search, $offset, $limit));
 });
 
+/**
+ * @OA\Get(path="/jobs/{id}",
+ *     @OA\Parameter(@OA\Schema(type="integer"), in="path", allowReserved=true, name="id", default=1),
+ *     @OA\Response(response="200", description="Get a job from database")
+ * )
+ */
 Flight::route('GET /jobs/@id', function($id){
     $jobService = new JobService();
     $job = $jobService->getById($id);
     Flight::json($job);
 });
 
+/**
+ * @OA\Post(path="/jobs",
+ *     @OA\Response(response="200", description="Add a job to database")
+ * )
+ */
 Flight::route('POST /jobs', function(){
     $request = Flight::request();
     $data = $request->data->getData();
@@ -19,6 +39,12 @@ Flight::route('POST /jobs', function(){
     Flight::json($data);
 });
 
+/**
+ * @OA\Put(path="/jobs/{id}",
+ *     @OA\Parameter(@OA\Schema(type="integer"), in="path", allowReserved=true, name="id", default=1),
+ *     @OA\Response(response="200", description="Update a job in the database")
+ * )
+ */
 Flight::route('PUT /jobs/@id', function($id){
     $request = Flight::request();
     $data = $request->data->getData();
