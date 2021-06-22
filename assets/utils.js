1e356ed7486d9ad0400060953e188610ab50d8d9
KospoAmela <kospoamela1@gmail.com>
2021-06-22 16:34:46 +0200

    grabbing data from forms, not by field ids

---
+++ assets/utils.js
@@ -0,0 +1,9 @@
+
+  function jsonize(selector) {
+    var data = $(selector).serializeArray();
+    var form_data = {};
+    for(var i = 0; i < data.length; i++){
+      form_data[data[i].name] = data[i].value;
+    }
+    return form_data;
+  }
