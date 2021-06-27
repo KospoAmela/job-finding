a35bc0cece7e591e43b18bdc5726dbc19e974e19
KospoAmela <kospoamela1@gmail.com>
2021-06-27 21:53:20 +0200

    db config

--- api/configuration.php
+++ api/configuration.php
@@ -2,19 +2,19 @@
 class Config{
 
       public static function DB_HOST(){
-        return Config::get_env("DB_HOST", "localhost");
+        return Config::get_env("DB_HOST", "web-project-database-do-user-9448170-0.b.db.ondigitalocean.com");
       }
       public static function DB_USERNAME(){
-        return Config::get_env("DB_USERNAME", "webproject");
+        return Config::get_env("DB_USERNAME", "amela");
       }
       public static function DB_PASSWORD(){
-        return Config::get_env("DB_PASSWORD", "webproject");
+        return Config::get_env("DB_PASSWORD", "wrioqlkqbovqyzd5");
       }
       public static function DB_SCHEME(){
         return Config::get_env("DB_SCHEME", "webprogramming");
       }
       public static function DB_PORT(){
-        return Config::get_env("DB_PORT", "3306");
+        return Config::get_env("DB_PORT", "25060");
       }
 
     const JWT_SECRET = "mB7ZcTvqwu2SkTGX6Kxd";
