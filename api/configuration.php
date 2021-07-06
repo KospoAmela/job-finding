8aa02cf8a3fd261424cf3a825d466aa5937b7e49
Amela <78913115+KospoAmela@users.noreply.github.com>
2021-07-06 22:52:44 +0200

    remotemysql.com

--- api/configuration.php
+++ api/configuration.php
@@ -2,19 +2,19 @@
 class Config{
 
       public static function DB_HOST(){
-        return Config::get_env("DB_HOST", "web-project-database-do-user-9448170-0.b.db.ondigitalocean.com");
+        return Config::get_env("DB_HOST", "remotemysql.com");
       }
       public static function DB_USERNAME(){
-        return Config::get_env("DB_USERNAME", "amela");
+        return Config::get_env("DB_USERNAME", "WUci3qINay");
       }
       public static function DB_PASSWORD(){
-        return Config::get_env("DB_PASSWORD", "wrioqlkqbovqyzd5");
+        return Config::get_env("DB_PASSWORD", "7C6ctqsXO0");
       }
       public static function DB_SCHEME(){
-        return Config::get_env("DB_SCHEME", "webprogramming");
+        return Config::get_env("DB_SCHEME", "WUci3qINay");
       }
       public static function DB_PORT(){
-        return Config::get_env("DB_PORT", "25060");
+        return Config::get_env("DB_PORT", "3306");
       }
 
     const JWT_SECRET = "mB7ZcTvqwu2SkTGX6Kxd";
